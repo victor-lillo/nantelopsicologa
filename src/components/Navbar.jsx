daf062dc5b01017b7555f8e43351b73eb6499e8a
unworking <victortic0910@gmail.com>
2022-04-27 18:38:59 +0200

    'commit'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import React, { useRef } from 'react'
 import Link from 'next/link'
-import Image from 'next/image';
+
 import { useRouter } from "next/router";
 
 // import logo from '../../public/logo.png';
-import logo from "../../public/assets/img/Logo.png";
 
 import Monogram from './Monogram';
 
