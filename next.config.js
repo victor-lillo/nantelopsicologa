5a51f0ab28c8094adf5e45f229a4fe663f06ed57
unworking <victortic0910@gmail.com>
2023-08-10 09:11:53 +0200

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  images: {
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  }
-  // ,
-  // webpack5: true,
-  // webpack: (config) => {
-  //   config.resolve.fallback = { fs: false, path: false, process: false };
-
-  //   return config;
-  // },
 }
 
+module.exports = nextConfig
